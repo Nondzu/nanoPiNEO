869180ac6f39ab56a7e87792642bbd837370b4c0
Nondzu <kamil.nonckiewicz@gmail.com>
2017-04-08 15:52:16 +0200

    qt tcp server

--- tcp_server/tcp_server.cpp
+++ tcp_server/tcp_server.cpp
@@ -5,3 +5,41 @@ tcp_server::tcp_server()
 
 }
 
+
+int tcp_server::init()
+{
+
+    int socket_desc , client_sock , c , read_size;
+    struct sockaddr_in server , client;
+    char client_message[2000];
+
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8180 );
+
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        //print the error message
+        perror("bind failed. Error");
+        return 1;
+    }
+    puts("bind done");
+
+    //Listen
+    listen(socket_desc , 3);
+
+
+
+
+    return 1;
+}
--- tcp_server/tcp_server.h
+++ tcp_server/tcp_server.h
@@ -1,12 +1,19 @@
 #ifndef TCP_SERVER_H
 #define TCP_SERVER_H
 
+#include<stdio.h>
+#include<string.h>    //strlen
+#include<sys/socket.h>
+#include<arpa/inet.h> //inet_addr
+#include<unistd.h>    //write
+#include <stdint.h>
 
 class tcp_server
 {
 public:
     tcp_server();
-
+    int init();
+    uint32_t port;
 
 };
 
